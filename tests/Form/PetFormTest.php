9ac9d8a7b29b97029a55d18ca35c7c6d703fd051
nikolina <nikolinap85@gmail.com>
2019-03-09 23:24:23 +0100

    Test - Pet::setPhotos() problem

--- tests/Form/PetFormTest.php
+++ tests/Form/PetFormTest.php
@@ -3,24 +3,60 @@
 namespace App\Tests\Form;
 
 use App\Entity\Pet;
+use App\Entity\Photo;
 use App\Entity\Species;
 use App\Entity\User;
 use App\Form\PetForm;
 use App\Form\PhotoTransformer;
 use App\Form\UserTransformer;
+use App\Kernel;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\PreloadedExtension;
 use Symfony\Component\Form\Test\TypeTestCase;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class PetFormTest extends TypeTestCase
 {
     private $photoTransformer;
     private $userTransformer;
     private $security;
+    /**
+     * @var Kernel
+     */
+    protected static $kernel;
+    protected static $container;
+
+    protected function get(string $serviceId)
+    {
+        return $this->getContainer()->get($serviceId);
+    }
+
+    protected function getContainer()
+    {
+        if (!isset(self::$kernel)) {
+            self::$kernel = new Kernel($_ENV['APP_ENV'], $_ENV['APP_DEBUG']);
+            self::$kernel->boot();
+        }
+
+        if (!isset(self::$container)) {
+            self::$container = self::$kernel->getContainer();
+        }
+
+        return self::$container;
+    }
 
     public function testSubmitValidData()
     {
+        $user = new User();
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $photo = new Photo();
+        $uploadedFile = new UploadedFile('/home/nikolina/Downloads/Olu pogled.jpg', 'Olu pogled.jpg');
+        $photo->setFile($uploadedFile);
+
         $formData = [
             'name' => "niki",
             'description' => 'opis',
@@ -28,7 +64,8 @@ class PetFormTest extends TypeTestCase
             'species' => new Species(),
             'gender' => 'MALE',
             'breed' => 'neka vrsta',
-            'photos' => new FileType()
+            'photos' => new ArrayCollection([$photo]),
+            'owner' => $user,
         ];
 
         $objectToCompare = new Pet();
@@ -66,12 +103,14 @@ class PetFormTest extends TypeTestCase
         parent::setUp();
     }
 
+
+
     protected function getExtensions()
     {
         $petForm = new PetForm($this->photoTransformer, $this->userTransformer, $this->security);
 
         return [
-            new PreloadedExtension([$petForm], [])
+            new PreloadedExtension([$petForm, new EntityType($this->get('doctrine'))], [])
         ];
     }
 }
